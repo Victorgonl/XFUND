52e9df2cadf6fe8052f90096911ff871051296bf
Victorgonl <victorgonl@outlook.com>
2024-04-15 13:31:09 +0000

    fix

--- xfund.py
+++ xfund.py
@@ -16,7 +16,6 @@ XFUND_DATA_FOLDER = f"{XFUND_FOLDER}data/"
 XFUND_IMAGE_FOLDER = f"{XFUND_FOLDER}image/"
 XFUND_CITATION_FILE = f"{ROOT_PATH}/xfund.bib"
 XFUND_LANGUAGES = ["zh", "de", "es", "fr", "it", "ja", "pt"]
-XFUND_LANGUAGES = ["pt"]
 
 INFO_NAME = "dataset_name"
 INFO_SPLITS = "splits"
@@ -36,14 +35,16 @@ def extract_data_from_json(data_file_directory):
     for document in json_data["documents"]:
         sample = []
         for document_entitie in document["document"]:
-            entity = {"id": document_entitie["id"],
+            entity = {
+                "id": document_entitie["id"],
                 "text": document_entitie["id"],
-                    "text": document_entitie["text"],
-                    "box": document_entitie["box"],
-                    "label": document_entitie["label"].upper(),
-                    "words": [],
-                    "boxes": [],
-                    "links": document_entitie["linking"]}
+                "text": document_entitie["text"],
+                "box": document_entitie["box"],
+                "label": document_entitie["label"].upper(),
+                "words": [],
+                "boxes": [],
+                "links": document_entitie["linking"],
+            }
             for word_box in document_entitie["words"]:
                 word = word_box["text"]
                 box = word_box["box"]
@@ -54,8 +55,9 @@ def extract_data_from_json(data_file_directory):
     data = sort_samples_data(data, sort_by_relations=True)
     return data
 
+
 def extract_zip(zip_directory, extract_directory):
-    with zipfile.ZipFile(zip_directory, 'r') as zip_ref:
+    with zipfile.ZipFile(zip_directory, "r") as zip_ref:
         zip_ref.extractall(extract_directory)
 
 
@@ -67,12 +69,12 @@ def save_data(data, data_directory):
 
 def xfund(languages=XFUND_LANGUAGES):
     if not all(language in XFUND_LANGUAGES for language in languages):
-        raise Exception(f"Language(s) not available on XFUND. Try these {XFUND_LANGUAGES}.")
+        raise Exception(
+            f"Language(s) not available on XFUND. Try these {XFUND_LANGUAGES}."
+        )
     os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
     os.makedirs(XFUND_DATA_FOLDER, exist_ok=True)
-    xfund_info = {INFO_NAME: "XFUND",
-                  INFO_SPLITS: {},
-                  INFO_CITATION: None}
+    xfund_info = {INFO_NAME: "XFUND", INFO_SPLITS: {}, INFO_CITATION: None}
     with open(XFUND_CITATION_FILE, "r") as xfund_citation:
         xfund_info["citation"] = xfund_citation.read()
     for language in languages:
@@ -92,9 +94,8 @@ def xfund(languages=XFUND_LANGUAGES):
         val_data = extract_data_from_json(f"{DOWNLOAD_FOLDER}/{val_json_file}")
         save_data(val_data, XFUND_DATA_FOLDER)
         extract_zip(f"{DOWNLOAD_FOLDER}/{val_zip_file}", XFUND_IMAGE_FOLDER)
-         # splits
-        xfund_info[INFO_SPLITS] = {"train": [],
-                            "val": []}
+        # splits
+        xfund_info[INFO_SPLITS] = {"train": [], "val": []}
         for train_sample_key in train_data.keys():
             xfund_info[INFO_SPLITS]["train"].append(train_sample_key)
         for val_sample_key in val_data.keys():
@@ -103,6 +104,6 @@ def xfund(languages=XFUND_LANGUAGES):
         json.dump(xfund_info, xfund_info_json, cls=UflaFormsJSONEncoder, indent=4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     languages_to_load = sys.argv[1:]
     xfund(languages=languages_to_load) if len(languages_to_load) else xfund()
